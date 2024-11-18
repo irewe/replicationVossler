efdea95096c7cc546a875426b53645b5ad259867
Ivana Rodriguez Ewerlöf <ivana.ewerlof@sva.se>
2024-11-18 15:57:01 +0100

    changes

--- R/dataprep.R
+++ R/dataprep.R
@@ -1,10 +1,10 @@
 library(data.table)
 
 data <- fread("data/dataVossler.tab")
-head(data[, .N , by = c("RespondentID", "ChoiceID")],100)
-data[, .N , by = c("BlockType", "Spatial_Unit", "Ncost")]
+head(data[, .N , by = c("RespondentID", "ChoiceID")], 100)
+data[, .N , by = c("BlockType", "Spatial_Unit", "NL", "Cost","Referendum")]
 
-data[, .N, by = "Ncost"]
+data[, .N, by = "WQ_Medium"]
 
 colnames(data)
-unique(data$NL)
+unique(data$HUC4)
