7ace9bb5014059955d5112b42d3901ed92f72d0f
Ivana Rodriguez Ewerlöf <ivana.ewerlof@sva.se>
2024-11-19 15:01:09 +0100

    trying to find combinations used

--- R/dataprep.R
+++ R/dataprep.R
@@ -1,10 +1,22 @@
 library(data.table)
 
 data <- fread("data/dataVossler.tab")
-head(data[, .N , by = c("RespondentID", "ChoiceID")], 100)
-data[, .N , by = c("BlockType", "Spatial_Unit", "NL", "Cost","Referendum")]
+head(data[, .N , by = c("RespondentID", "ChoiceID")], 40)
+data[, .N , by = c("BlockType", "Spatial_Unit", "NL","ASC")]
+data[, .N , by = c("BlockType", "Spatial_Unit", "NL", "Cost", "ASC", "Referendum")]
+data[, .N , by = c("BlockType", "Spatial_Unit", "NL")]
 
-data[, .N, by = "WQ_Medium"]
+data$WQ_HUC8_b <- ifelse(data$WQ_HUC8 == 0, 0, 1)
+data$WQ_HUC4_b <- ifelse(data$WQ_HUC4 == 0, 0, 1)
+data$WQ_Medium_b <- ifelse(data$WQ_Medium == 0, 0, 1)
+data$WQ_Large_b <- ifelse(data$WQ_Large == 0, 0, 1)
+
+combos <- 
+data[, .N , by = c(
+  "BlockType", "ASC",
+  "Spatial_Unit", "NL", "Cost",
+  "WQ_HUC4_b", "WQ_Medium_b", "WQ_Large_b")]
+
 
 colnames(data)
-unique(data$HUC4)
+unique(data$WQ_HUC4)
