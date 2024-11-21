1c997e2b24f841cdfaa1f0c120b3a74661054a75
Ivana Rodriguez Ewerlöf <ivana.ewerlof@sva.se>
2024-11-21 15:19:15 +0100

    more columns in wide data

--- R/make_wide_data.R
+++ R/make_wide_data.R
@@ -4,24 +4,27 @@ library(data.table)
 ## Read data
 data <- fread("data/dataVossler.tab")
 
-data <- data[!is.na(Vote), c(
-  "RespondentID", "ChoiceID", "Referendum", "Vote", "Cost", "ASC", "WQ_HUC8", "WQ_HUC4",
-  "WQ_Medium", "WQ_Large", "WQ_HUC4_NL", "WQ_Medium_NL")]
-
+data <- data[!is.na(Vote)]
 data <- data[order(ChoiceID, ASC)]
-
-data
 
 data_wide <- dcast(
   data,
-  ChoiceID + RespondentID + Referendum ~ ASC,                 # Reshape by Alternative
+  ChoiceID + RespondentID + Referendum +
+  HUC8BCG + HUC4BCG_base + HUC4BCG_scenario1 +
+  HUC4BCG_scenario2 + MediumBCG_base + MediumBCG_scenario1 +
+  MediumBCG_scenario2 + HUC4BCG_base_NL + HUC4BCG_scenario2_NL +
+  MediumBCG_base_NL + MediumBCG_scenario2_NL ~ ASC,                 # Reshape by Alternative
   value.var = c(
     "Vote", "Cost",
-    "WQ_HUC8", "WQ_HUC4", "WQ_Medium", "WQ_Large", "WQ_HUC4_NL", "WQ_Medium_NL")  # Columns to spread
+    "WQ_HUC8", "WQ_HUC4", "WQ_Medium", "WQ_Large", "WQ_HUC4_NL", "WQ_Medium_NL",
+    "ASC_PercentInstate", "ASC_PercentInstate_NL")  # Columns to spread
 )
 
 data_wide$choice <- data_wide$Vote_1
 data_wide$Vote_0 <- NULL
 data_wide$Vote_1 <- NULL
+data_wide$ASC_0 <- 0
+data_wide$ASC_1 <- 1
+
 
 fwrite(data_wide, "data/dataVossler_wide.csv")
